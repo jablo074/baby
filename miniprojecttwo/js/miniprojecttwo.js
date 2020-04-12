3d5a6e45ee95715209c6af499499681876febbfe
jablo074 <60587413+jablo074@users.noreply.github.com>
2020-04-12 14:44:41 -0500

    Add files via upload

---
+++ miniprojecttwo/js/miniprojecttwo.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+
+$(".container").click(function() {
+
+	$(".baby2").addClass("animate");
+	$(".baby1").addClass("animate");
+	$(".food").addClass("animate1");
+
+
+});
+
+
+
+
+});
